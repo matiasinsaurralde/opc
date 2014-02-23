2272a8cca6aac1af2c43fcd7148c81972e244db7
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-02-23 06:34:58 -0300

    a good start :)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 // open proxy crawler
 
 import "net/http"
+import "net/url"
 import "fmt"
 
 func pageHandler( w http.ResponseWriter, r *http.Request ){
@@ -14,8 +15,23 @@ func pageHandler( w http.ResponseWriter, r *http.Request ){
   }
 }
 
+func crawl() {
+  fmt.Println( "i'm crawling the world" )
+  someProxy := "201.217.55.97:3128"
+  testProxy( someProxy )
+}
+
+func testProxy(proxyAddress string) {
+  proxyUrl, _ := url.Parse("http://"+proxyAddress)
+  testClient := &http.Client{ Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)} }
+  response, err := testClient.Get("http://www.google.com/robots.txt")
+  fmt.Println( err )
+  fmt.Println( response )
+}
+
 func main() {
   fmt.Println( "up and running!" )
+  go crawl()
   http.HandleFunc( "/", pageHandler )
   http.ListenAndServe( ":8000", nil )
 }
